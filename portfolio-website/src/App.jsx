beedb145de861331d4e3bbbe2645b0b902f5d3d2
tsk-balaji <tskbalaji15@gmail.com>
2025-01-24 02:34:38 +0530

    Fix import path for Navbar component in App.jsx

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,6 +1,6 @@
 import "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import { ToastContainer } from "react-toastify";
